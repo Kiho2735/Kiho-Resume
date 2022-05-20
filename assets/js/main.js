08565d77ff3f56301c661ff65dd4bb08bc18f025
Kiho <kiho2735@icloud.com>
2022-05-20 17:00:00 -0400

    Added mock up
    Reference : https://www.youtube.com/watch?v=AKNvTxWOdKw

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// =========== MENU SHOW ===========
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+};
+
+showMenu("nav-toggle", "nav-menu");
